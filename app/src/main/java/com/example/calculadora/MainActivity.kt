518e04f7db448963b12eed49d71c26ba7f289610
larissacarletti <larissalealcarletti@gmail.com>
2023-01-11 19:05:54 -0300

    Initial commit

---
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.calculadora
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.button.MaterialButtonToggleGroup
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val firstDigit = findViewById<EditText>(R.id.tnPrimeiro)
+        val secondDigit = findViewById<EditText>(R.id.tnSegundo)
+        val buttonSum = findViewById<Button>(R.id.btnSoma)
+        val buttonMenos = findViewById<Button>(R.id.btnSub)
+        val buttonMulti = findViewById<Button>(R.id.btnMulti)
+        val buttonDiv = findViewById<Button>(R.id.btnDiv)
+        val resultado = findViewById<EditText>(R.id.tnResultado)
+
+        val numero1 = firstDigit.text.toString()
+        val numero2 = secondDigit.text.toString()
+
+        buttonSum.setOnClickListener {
+            resultado
+        }
+
+
+
+
+
+
+    }
+}
