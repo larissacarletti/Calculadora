5179529e6dcecf4d1415442f3830f21895aae8b2
larissacarletti <larissalealcarletti@gmail.com>
2023-01-12 14:52:50 -0300

    atualização

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,32 +1,64 @@
 package com.example.calculadora
 
-import android.annotation.SuppressLint
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import com.google.android.material.button.MaterialButtonToggleGroup
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val firstDigit = findViewById<EditText>(R.id.tnPrimeiro)
         val secondDigit = findViewById<EditText>(R.id.tnSegundo)
         val buttonSum = findViewById<Button>(R.id.btnSoma)
-        val buttonMenos = findViewById<Button>(R.id.btnSub)
-        val buttonMulti = findViewById<Button>(R.id.btnMulti)
-        val buttonDiv = findViewById<Button>(R.id.btnDiv)
-        var resultado = findViewById<EditText>(R.id.tnResultado)
+        val buttonSubstraction = findViewById<Button>(R.id.btnSub)
+        val buttonMultiplication = findViewById<Button>(R.id.btnMulti)
+        val buttonDivision = findViewById<Button>(R.id.btnDiv)
+        val resultado = findViewById<TextView>(R.id.tvOperacao)
 
-        val numero1 = firstDigit.text.toString()
-        val numero2 = secondDigit.text.toString()
+        var numero1 : Int
+        var numero2 : Int
+        var result : Int
 
+        buttonSum.setOnClickListener {
+            numero1 = firstDigit.text.toString().toInt()
+            numero2 = secondDigit.text.toString().toInt()
 
+            result = numero1 + numero2
+            resultado.text = result.toString()
 
+            }
 
+        buttonSubstraction.setOnClickListener {
+                numero1 = firstDigit.text.toString().toInt()
+                numero2 = secondDigit.text.toString().toInt()
 
+                result = numero1 - numero2
+                resultado.text = result.toString()
+            }
 
+        buttonMultiplication.setOnClickListener {
+                numero1 = firstDigit.text.toString().toInt()
+                numero2 = secondDigit.text.toString().toInt()
+
+                result = numero1 * numero2
+                resultado.text = result.toString()
+
+            }
+
+        buttonDivision.setOnClickListener {
+                numero1 = firstDigit.text.toString().toInt()
+                numero2 = secondDigit.text.toString().toInt()
+
+                result = (numero1 / numero2)
+                resultado.text = result.toString()
+
+            }
     }
+
 }
